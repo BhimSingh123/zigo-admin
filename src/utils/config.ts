f36df942f99aa197e69c1675c68bba460ae01dcb
singhBhim <bhim.singh@futureprofilez.com>
2026-07-12 10:58:15 +0530

    new ciode

---
+++ src/utils/config.ts
@@ -0,0 +1,11 @@
+export const baseURL: string = "https://superstreamapi.stackearn.com/";
+export const key: string = "";
+export const projectName: string = "Zigo Live ";
+export const apiKey: string =  "AIzaSyAv43NGBQ-aUX8fmnu-vK9uOqUTdFy_9DA";
+export const authDomain : string =  "superstreamapp.firebaseapp.com";
+export const projectId : string = "superstreamapp";
+export const storageBucket : string = "superstreamapp.firebasestorage.app";
+export const messagingSenderId : string = "353610721060";
+export const appId : string = "1:353610721060:web:8e2c2421c3aca4c013b5b1";
+export const measurementId : string = "G-HZ87J5T08V"
+
